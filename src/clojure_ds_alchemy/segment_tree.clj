74b3bf32e673861361867f2338e11724bcd88006
Tirpitz9992 <tirpitz9992@gmail.com>
2024-01-08 15:45:27 +0800

    fixed

---
+++ src/clojure_ds_alchemy/segment_tree.clj
@@ -0,0 +1,13 @@
+(ns clojure-ds-alchemy.segment-tree)
+
+(defn build-segment-tree
+  "建树"
+  [arr start end]
+  (if (= start end)
+    {:value start :left nil :right nil}
+    (let [mid (quot (+ start end) 2)
+          left-child (build-segment-tree arr start mid)
+          right-child (build-segment-tree arr (inc mid) end)]
+      {:value (+ (:value left-child) (:value right-child))
+       :left left-child
+       :right right-child})))
