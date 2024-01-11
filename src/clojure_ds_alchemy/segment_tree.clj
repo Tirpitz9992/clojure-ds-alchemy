ada4a90d5f051c327267af36f6b954f0d9b8d5d5
Tirpitz9992 <tirpitz9992@gmail.com>
2024-01-11 15:30:56 +0800

    segmenttree

--- src/clojure_ds_alchemy/segment_tree.clj
+++ src/clojure_ds_alchemy/segment_tree.clj
@@ -7,8 +7,22 @@
     {:value start :left nil :right nil}
     (let [mid (quot (+ start end) 2)
           left-child (build-segment-tree arr start mid)
-          right-child (build-segment-tree arr (inc mid) end)]
+          right-child (build-segment-tree arr (inc mid) end)] ;向下递归
       {:value (+ (:value left-child) (:value right-child))
        :left left-child
        :right right-child})))
 
+(defn query-range
+  "查询给定区间的和"
+  [node start end qs qe]
+  (if (or (< qe start) (> qs end))
+    0 ;如果需要查询qe < start 就是说在查询的区间外的话
+    (if (and (<= qs start) (>= qe end))
+      (:value node)
+      (let [mid (quot (+ start end) 2)]
+        (+ (query-range (:left node) start mid qs qe)
+           (query-range (:right node) (inc mid) end qs qe))))))
+
+(defn update-segment-tree
+  [node  ]
+  )
